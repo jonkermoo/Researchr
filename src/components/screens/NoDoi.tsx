c5b6dba049f62878e290459417a8ea4112dd9c25
Johnny Zhu <zhu.johnny0@gmail.com>
2025-07-10 04:33:18 -0400

    removed inline script

--- src/components/screens/NoDoi.tsx
+++ src/components/screens/NoDoi.tsx
@@ -3,15 +3,13 @@ import Logo from "../../assets/images/logo.png";
 export default function Welcome() {
   return (
     <div className="flex flex-col justify-center items-center gap-[25px] h-full">
-      <div className="text-[16px]" style={{ fontFamily: "Hind, sans-serif" }}>
+      <div className="text-[16px] font-hind">
         Researchr does not work on this site
       </div>
       <div>
         <img src={Logo} alt="Researchr Logo" className="w-[75px] h-auto" />
       </div>
-      <div className="text-[14px]" style={{ fontFamily: "Hind, sans-serif" }}>
-        Try a site on Google Scholars
-      </div>
+      <div className="text-[14px] font-hind">Try a site on Google Scholars</div>
     </div>
   );
 }
